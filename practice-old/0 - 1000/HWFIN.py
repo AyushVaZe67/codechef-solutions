75185fb3cc05919f94a42e8419a29f2c65692c1d
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2025-11-26 19:26:33 +0530

    Add files via upload

---
+++ practice-old/0 - 1000/HWFIN.py
@@ -0,0 +1,7 @@
+# cook your dish here
+x , y = map(int, input().split())
+if (x + y * 10) >= 100:
+    print('YES')
+else:
+    print('NO')
+    
---
+++ practice-old/0 - 1000/RECSQ.py
@@ -0,0 +1,6 @@
+# cook your dish here
+a,b,c = map(int, input().split())
+if a * b == c ** 2:
+    print('YES')
+else:
+    print('NO')
